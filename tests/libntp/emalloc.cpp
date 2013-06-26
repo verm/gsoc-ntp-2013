e00761087d2683344f4475841b23103fa60e6dc8
kernel9709 <kernel9709@gmail.com>
2013-06-26 11:54:55 +0800

    UT 4. add ut cases for libntp/emalloc.c
    Create test case for estrdup_impl in non-debug mode.

---
+++ tests/libntp/emalloc.cpp
@@ -0,0 +1,56 @@
+/* This file contains test for libntp/emalloc.c
+   There are 2 main apis for emalloc.c, one is emalloc, 
+   the other is estrdup_impl. emalloc() can be tested by other ut cases. */
+
+#include "libntptest.h"
+
+extern "C" {
+#ifdef OPENSSL
+# include "openssl/err.h"
+# include "openssl/rand.h"
+# include "openssl/evp.h"
+#endif
+#include "ntp.h"
+#include "ntp_stdlib.h"
+};
+
+
+class emallocTest : public libntptest {
+protected:
+
+	virtual void SetUp() {
+        return;
+	}
+
+};
+
+/* Note, if defined EREALLOC_CALLSITE, that means we are using debug mode.
+   In fact, there is no need to test string dump in debug mode. */
+TEST_F(emallocTest, TestStringDump) {
+#ifndef EREALLOC_CALLSITE
+    const char *teststr = "TestStringDump";
+    char *newstring;
+	size_t	bytes;
+    int i,res;
+
+	bytes = strlen(teststr) + 1;
+
+    newstring = estrdup_impl(teststr);
+
+    /* if newstring == NULL, program will exit automatically,
+       so don't need to worry about that case */
+    res=0;
+    for (i=0; i<bytes; ++i)
+    {
+        if (newstring[i] != teststr[i])
+        {
+            res = 1;
+            break;
+        }
+    }
+    
+    /* confirm that every byte has been copied correctly */
+	EXPECT_EQ(res, 0);
+#endif
+}
+
