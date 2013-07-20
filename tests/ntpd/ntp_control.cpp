282940835165ba7241dda8ef3192c69236fab537
kernel9709 <kernel9709@gmail.com>
2013-07-20 19:17:21 +0800

    Update ntp_control.c test cases

--- tests/ntpd/ntp_control.cpp
+++ tests/ntpd/ntp_control.cpp
@@ -2,15 +2,10 @@
  * ntp_control.cpp - respond to mode 6 control messages and send async
  *		   traps.  Provides service to ntpq and others.
  */
-#include "libntptest.h"
+#include "ntpdtest.h"
      
- /* these following segments are copied from ntp_config.c */
- extern "C" {
-
-#ifdef HAVE_CONFIG_H
-# include <config.h>
-#endif
-
+/* these following segments are copied from ntp_config.c */
+extern "C" {
 #include <stdio.h>
 #include <ctype.h>
 #include <signal.h>
@@ -66,14 +61,13 @@ TEST_F(ntpControlTest, InitControl) {
 
     /* check values in the struct */
 	EXPECT_EQ(0, ctl_auth_keyid);
-	EXPECT_EQ(EVNT_UNSPEC, ctl_sys_last_event);
-	EXPECT_EQ(0, ctl_sys_num_events);
 	EXPECT_EQ(0, num_ctl_traps);
 }
 
 /* Test Control peer status */
 TEST_F(ntpControlTest, ConfigPeerStatus) {  
-    struct peer p;
+    static struct peer p;
+    static struct peer* pp;
     u_short res;
     
     p.status = 0;
@@ -82,13 +76,13 @@ TEST_F(ntpControlTest, ConfigPeerStatus) {
     p.reach = 1;
     p.cast_flags = MDF_TXONLY_MASK;
 
-    res = ctlpeerstatus(&p);
+    pp = &p;
+    res = ctlpeerstatus(pp);
 
     /* check values of status */
 	EXPECT_EQ(CTL_PST_CONFIG|
 	          CTL_PST_AUTHENABLE|
-	          CTL_PST_AUTHENTIC|
-	          CTL_PST_REACH|
+	          CTL_PST_AUTHENTIC|	          
 	          CTL_PST_BCAST,
 	          CTL_PEER_STATVAL(res));
     
@@ -97,45 +91,49 @@ TEST_F(ntpControlTest, ConfigPeerStatus) {
 /* Test system status control */
 TEST_F(ntpControlTest, ControlSysStatus) {  
     struct peer *prepeer;
-    struct peer p;
+    static struct peer p;
     u_short res;
 
     prepeer = sys_peer;
     sys_peer = &p;    
-    sys_peer->sstclktype = CTL_SST_TS_NTP;
-    
+#ifdef REFCLOCK
+    sys_peer->sstclktype = CTL_SST_TS_ATOM; 
+#endif
+
     res = ctlsysstatus();
 
     /* check values of system source */
-	EXPECT_EQ(CTL_SST_TS_NTP, CTL_SYS_SOURCE(res));
+#ifdef REFCLOCK
+	EXPECT_EQ(CTL_SST_TS_ATOM, CTL_SYS_SOURCE(res & 0xffffffff));
+#else
+    EXPECT_EQ(CTL_SST_TS_NTP, CTL_SYS_SOURCE(res & 0xffffffff));
+#endif
 
     sys_peer = prepeer;
 }
 
-/* Test system status control */
-TEST_F(ntpControlTest, ControlSysStatus) {  
-    struct ctl_var *pre_sys_var;
+/* Test system var control */
+TEST_F(ntpControlTest, ControlSysVar) {  
+    struct ctl_var *pk;
+    struct ctl_var **ppk;
     struct ctl_var k[2];
-    char   *strtest = "test=123";
-    char   *tag = "test", *res;
+    char   strtest[] = "test=123";
+    char   tag[] = "test";
+    const char *res;
+    const char *expres = "123";
     u_long  size = 9;
     u_short def= 1;
 
-    pre_sys_var = ext_sys_var;
-    ext_sys_var = k;
-    k[0].text = "test=abc"
+    k[0].text = (char *)emalloc(size);
+    strcpy(k[0].text, "test=abc");
     k[0].flags = 0;
-    k[0].next = &(k[1]);
     k[1].flags = EOV;
-    k[1].next = NULL;
-        
+    pk = &k[0];
+    ppk = &pk;
+
     set_sys_var(strtest, size, def);
     res = get_ext_sys_var(tag);
 
     /* check values of system source */
-	EXPECT_STREQ("123", res);
-
-    ext_sys_var = pre_sys_var;
+	EXPECT_STREQ(expres, res);
 }
-
-
